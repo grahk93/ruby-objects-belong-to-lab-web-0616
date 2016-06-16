abd57505325f9aa60150546a173423b6113077f8
grahk93 <grace.juster@flatironschool.com>
2016-06-16 18:04:37 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,14 @@
+class Artist
+  attr_accessor :name 
+  # def initialize
+  #   @songs = []
+  # end 
+
+  # def add_song(song_obj)
+  #   @songs << song_obj
+  #   song_obj.artist = self
+  # end 
+end 
+
+#songs belong to artist 
+#artists have many songs 
